97adabf07fbadf728e5a1b8b84555ee2c2664024
Raghu_Vardhan <vardhanvsr2004@gmail.com>
2024-10-27 09:16:39 +0530

    add routes to index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+
+const teacherRoutes = require("./routes/teacherRoutes");
+const assignmentRoutes = require("./routes/assignmentRoutes");
+const contentRoutes = require("./routes/contentRoutes");
+const courseRoutes = require("./routes/courseRoutes");
+const quizRoutes = require("./routes/quizRoute");
+const studentRoutes = require("./routes/studentRoutes");
+const submissiosRoutes = require("./routes/submissionRoutes");
+const videoRoutes = require("./routes/videoRoutes");
+
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
@@ -14,6 +24,15 @@ const port = process.env.PORT_NUMBER || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/teacher", teacherRoutes);
+app.use("/student", studentRoutes);
+app.use("/course", courseRoutes);
+app.use("/video", videoRoutes);
+app.use("/content", contentRoutes);
+app.use("/submission", submissiosRoutes);
+app.use("/assignment", assignmentRoutes);
+app.use("/quiz", quizRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
